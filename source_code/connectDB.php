3b8c4e22cc0aa80cd7c5114297ecf7b7c321785c
joloses <jolos@student.ubc.ca>
2024-04-11 22:24:39 -0700

    Fixed hashed passwords to match requirements.

--- source_code/connectDB.php
+++ source_code/connectDB.php
@@ -1,16 +1,16 @@
 <?php
 // // CONNECT TO LOCAL DATABASE
 
-/*
+///*
 $host = "localhost";
 $database = "DDL360";
 $user = "webuser";
 $password = "P@ssw0rd";
 $connection = mysqli_connect($host, $user, $password, $database);
-*/
+//*/
 
 // // CONNECT TO JOLO'S WEBSERVER DATABASE 
-  // /*
+  /*
   $host = "localhost";
   $database = "db_85456473";
   $user = "85456473";
